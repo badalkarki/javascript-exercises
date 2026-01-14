e840def5698b790f7bd9a4f7ea61fb41cbad4923
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-01-14 21:21:37 +0545

    Creates function to reverse the string
    using TDD ~ Test Driven Development.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    return words.split("").reverse().join("");
 };
+reverseString("Hello, Badal.");
+
 
 // Do not edit below this line
 module.exports = reverseString;
