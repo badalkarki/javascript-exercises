b06c194cfe79dd05f5303927a8f88c2cd6a344cc
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-01-25 19:15:08 +0545

    Sum all values between the arguments provided
    This function sum all the values that comes inside the
    two arguments and return the sum.
    
    In this problem I get to know about the Number.isInteger() methods which
    is used on Number which is the global built-in thing for all numbers.
    Also strength my knowledge on if-else statement and the operators.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(value1,value2) {
+    let sum = 0;
+    if (value1 < 0 || value2 < 0) {
+       return "ERROR";
+    }else if (typeof value1 !== "number" || typeof value2 !== "number") {
+        return "ERROR";
+    } else if (!Number.isInteger(value1) || !Number.isInteger(value2)) {
+        return "ERROR";
+    }
+    else {
+    if (value1 < value2) {
+        for (let i = value1 ; i <= value2; i++){
+            sum += i;
+        }
+    } else {
+        for (let i = value1; i>=value2; i--) {
+            sum += i;
+        }
+    }
+    return sum;
+}
 };
+
+console.log(sumAll(4,1));
 
 // Do not edit below this line
 module.exports = sumAll;
