4cd23813784d87df622476a8a60f7bb07fd46085
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-11 18:22:30 +0545

    Adds more refined logic for palindromes
    which uses filter method to eradicate the punctuation and insures
    it only contains the lowercase alphabates and number. Also this is
    dynamic which automatically delete any punctuation(which previous logic wasn't able to do)

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,10 +1,24 @@
-const palindromes = function (words) {
-    const onlyLetters = words.toLowerCase().replaceAll(" ", "").replaceAll("!", "").replaceAll(",", "").replaceAll(".", "");
-    const spacelessLetter = words.toLowerCase().replaceAll(" ", "").replaceAll("!", "").replaceAll(",", "").replaceAll(".", "").split("");
-    const reverseWord= spacelessLetter.reverse().join("");
-    console.log(reverseWord)
-    return ( onlyLetters == reverseWord);
+// const palindromes = function (words) {
+//     const onlyLetters = words.toLowerCase().replaceAll(" ", "").replaceAll("!", "").replaceAll(",", "").replaceAll(".", "");
+//     const spacelessLetter = words.toLowerCase().replaceAll(" ", "").replaceAll("!", "").replaceAll(",", "").replaceAll(".", "").split("");
+//     const reverseWord= spacelessLetter.reverse().join("");
+//     console.log(reverseWord)
+//     return ( onlyLetters == reverseWord);
+// };
+
+const palindromes = function (string) {
+  const validAlpha = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  const cleanedChar = string.toLowerCase().split("").filter((character) => {
+    return validAlpha.includes(character);
+  });
+  console.log(cleanedChar)
+  const cleanedString = cleanedChar.join("");
+  const reverseString = cleanedChar.reverse().join("").toLowerCase();
+  return cleanedString == reverseString;
 };
+
+palindromes("A car, a man, a maraca.");
 palindromes("racecar!");
 // Do not edit below this line
 module.exports = palindromes;
