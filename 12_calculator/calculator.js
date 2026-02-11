3d46a9565e593a0632783bf04b45f6ab1a52f2d0
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-11 10:54:28 +0545

    simple calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+console.log(add(0, 0));
+
+const subtract = function (num1, num2) {
+  return num1 - num2;
+};
+subtract(2, 6);
+
+const sum = function (arr) {
+  return arr.reduce((acc, nextNum) => acc + nextNum, 0);
 };
 
-const subtract = function() {
-	
+sum([7, 11]);
+
+const multiply = function (arr) {
+  return arr.reduce((acc, nextValue) => acc * nextValue, 1);
+};
+multiply([2, 3]);
+
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const sum = function() {
-	
-};
+const factorial = function (num) {
+  // let factorial = 1;
+  if (num == 0) {
+    return 1;
+  } else if (num == 1) {
+    return 1;
+  } else {
+    for (let i = num - 1; i >= 1; i--) {
+     num *= i;
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+    }
+    return num;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
