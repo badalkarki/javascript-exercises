cd57a3762e2190d3806dcd30c39dec417f854289
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-11 14:35:13 +0545

    Identify the palindrome of simple word

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const spacelessLetter = words.replaceAll(" ", "").split("");
+    const reverseWord= spacelessLetter.reverse().join("");
+    console.log(reverseWord)
+    return (words == reverseWord);
 };
-
+palindromes("racecar!");
 // Do not edit below this line
 module.exports = palindromes;
