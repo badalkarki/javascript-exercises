06207a87932247a94cc8e23266e5b7a920fbd9ad
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-12 14:11:31 +0545

    Creates logic to find the oldest among objets of array

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce((acc, initialObj) => {
+    let accAge;
+    let initialObjAge;
+    if (!acc.yearOfDeath && !initialObj.yearOfDeath) {
+      accAge = new Date().getFullYear() - acc.yearOfBirth;
+      initialObjAge = new Date().getFullYear() - initialObj.yearOfBirth;
+    } else if (!acc.yearOfDeath) {
+      accAge = new Date().getFullYear() - acc.yearOfBirth;
+      initialObjAge = initialObj.yearOfDeath - initialObj.yearOfBirth;
+    } else if (!initialObj.yearOfDeath) {
+      accAge = acc.yearOfDeath - acc.yearOfBirth;
+      initialObjAge = new Date().getFullYear() - initialObj.yearOfBirth;
+    } else {
+      accAge = acc.yearOfDeath - acc.yearOfBirth;
+      initialObjAge = initialObj.yearOfDeath - initialObj.yearOfBirth;
+    }
 
+    if (accAge > initialObjAge) {
+      return acc;
+    } else {
+      return initialObj;
+    }
+  });
 };
 
 // Do not edit below this line
