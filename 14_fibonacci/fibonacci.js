0aa3bf0a47d213ef002449e826391cfbb55f55f5
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-12 09:26:29 +0545

    Create logic to give fibonacci number of specific place

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const fibonacciArr = [];
+    if(num === 0 || num === "0") return 0;
+    if(num < 0) return "OOPS";
+    for(let i = 0; i < num; i++){
+        if(fibonacciArr.length == 0 || fibonacciArr.length == 1) {
+            fibonacciArr.push(1);
+        } else{
+            let nextNum = fibonacciArr[fibonacciArr.length - 1] + fibonacciArr[fibonacciArr.length - 2];
+            fibonacciArr.push(nextNum)
+        }
+    }
+    return fibonacciArr.pop();
 };
+console.log(fibonacci(0));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
