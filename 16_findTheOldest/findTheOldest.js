4d323449d08554886eb59f4a86ea20feb07c25c1
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-12 14:27:23 +0545

    Use ternary operator for better readability

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,20 +1,15 @@
 const getAge = function (death, birth) {
-    if(!death) {
-        death = new Date().getFullYear();
-    }
-    return death - birth;
-}
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
 const findTheOldest = function (arr) {
   return arr.reduce((acc, initialObj) => {
     let accAge = getAge(acc.yearOfDeath, acc.yearOfBirth);
     let initialObjAge = getAge(initialObj.yearOfDeath, initialObj.yearOfBirth);
-
-    if (accAge > initialObjAge) {
-      return acc;
-    } else {
-      return initialObj;
-    }
+   return  accAge > initialObjAge ? acc : initialObj;
   });
 };
 
